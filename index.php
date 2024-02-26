9709b6122ae23b478eb678501ae8453c3358b0b5
Simon <simon@bitcartel.com>
2024-02-26 00:37:03 -0800

    Update for scash

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 $time_start = microtime(true); 
 
-  $MAIN_ENDPOINT = "http://127.0.0.1:8332/";
-  $TEST_ENDPOINT = "http://127.0.0.1:18332/";
-  define("BASE_URL", "/explorer/"); #set to absolute URL, use "/" if in root directory
+  $MAIN_ENDPOINT = "http://127.0.0.1:8342/"; // SCASH PORT
+  $TEST_ENDPOINT = "http://127.0.0.1:18342/"; // SCASH PORT
+  define("BASE_URL", "/"); #set to absolute URL, use "/" if in root directory
   define("USE_MOD_REWRITE", true);
-	define("RPC_USER", "bitcoin");
-	define("RPC_PASS", "CHANGEME");
+	define("RPC_USER", "user"); // CHANGE THIS
+	define("RPC_PASS", "password"); // CHANGE THIS
 	define("BLOCK_TXDETAILS", true); #blocks load faster without txdetails
 	define("TXHISTORY_MAX_BLOCKS_PER_CALL", 15); #how many blocks to process per call loop
 	
   // enable mod rewrite mode to have nice URLs like /block/<hash> instead of index.php?block=<hash>
-  $FOOTER = '<a href="https://github.com/jonasschnelli/simple-block-explorer">Simple Block Explorer</a> for Bitcoin Core';
+  $FOOTER = '<a href="https://github.com/scash-project/simple-block-explorer">Simple Block Explorer</a> for Scash';
   $TITLE = "Block Explorer";
 ###### END CONFIG SECTION
 
@@ -150,7 +150,7 @@ $time_start = microtime(true);
 					$data = rpcFetch("scantxoutset", '["start", ["addr('.$search.')"]]');
 					if(!is_null($data->result)) {
 						// ajax response for the scantxoutset report
-						echo "<hr /><div class=\"totalamount\"><b>Total unspent BTC:</b> ".$data->result->total_amount."</div><br>\n";
+						echo "<hr /><div class=\"totalamount\"><b>Total unspent SCASH:</b> ".$data->result->total_amount."</div><br>\n";
 						echo '<h4>UTXOs (unspent transaction outputs)</h4>'."\n";
 						echo '<table class="table table-bordered table-hover table-striped utxos">'."\n";
 						echo "<tr><th>Amount</th><th>TXID</th><th>Vout</th></tr>\n";
@@ -286,6 +286,7 @@ $time_start = microtime(true);
     $hash = $tophash;
     for ($i=0;$i<$blk_count;$i++) {
       array_push($lastblocks, getBlockJSON($hash));
+      if (empty($lastblocks[count($lastblocks)-1])) { continue; } // Fix null bug when chain only has genesis block
       $hash = $lastblocks[count($lastblocks)-1]->previousblockhash;
       $age_in_s = $NOW - $lastblocks[count($lastblocks)-1]->time;
       if ($age_in_s > 3600) {
@@ -423,7 +424,7 @@ $time_start = microtime(true);
     return BASE_URL."index.php";
   }
   function btcamount($amount) {
-    return number_format($amount, 8, ".", "'")." BTC";
+    return number_format($amount, 8, ".", "'")." SCASH";
   }
 	function shortHash($hash) {
 		if(strlen($hash) < 12) {
@@ -520,12 +521,7 @@ tr.highlight {background-color: powderblue;}
 	  <div class="container-fluid">
 				<div id="logo">
 				<a class="navbar-brand" href="<?php echo homelink(); ?>">
-					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bitcoinlogo me-2" viewBox="0 0 64 64">
-						<g transform="translate(0.00630876,-0.00301984)">
-						<path fill="#FFF" d="m63.033,39.744c-4.274,17.143-21.637,27.576-38.782,23.301-17.138-4.274-27.571-21.638-23.295-38.78,4.272-17.145,21.635-27.579,38.775-23.305,17.144,4.274,27.576,21.64,23.302,38.784z"/>
-						<path fill="#000" d="m46.103,27.444c0.637-4.258-2.605-6.547-7.038-8.074l1.438-5.768-3.511-0.875-1.4,5.616c-0.923-0.23-1.871-0.447-2.813-0.662l1.41-5.653-3.509-0.875-1.439,5.766c-0.764-0.174-1.514-0.346-2.242-0.527l0.004-0.018-4.842-1.209-0.934,3.75s2.605,0.597,2.55,0.634c1.422,0.355,1.679,1.296,1.636,2.042l-1.638,6.571c0.098,0.025,0.225,0.061,0.365,0.117-0.117-0.029-0.242-0.061-0.371-0.092l-2.296,9.205c-0.174,0.432-0.615,1.08-1.609,0.834,0.035,0.051-2.552-0.637-2.552-0.637l-1.743,4.019,4.569,1.139c0.85,0.213,1.683,0.436,2.503,0.646l-1.453,5.834,3.507,0.875,1.439-5.772c0.958,0.26,1.888,0.5,2.798,0.726l-1.434,5.745,3.511,0.875,1.453-5.823c5.987,1.133,10.489,0.676,12.384-4.739,1.527-4.36-0.076-6.875-3.226-8.515,2.294-0.529,4.022-2.038,4.483-5.155zm-8.022,11.249c-1.085,4.36-8.426,2.003-10.806,1.412l1.928-7.729c2.38,0.594,10.012,1.77,8.878,6.317zm1.086-11.312c-0.99,3.966-7.1,1.951-9.082,1.457l1.748-7.01c1.982,0.494,8.365,1.416,7.334,5.553z"/>
-						</g>
-					</svg>
+				🌐 <!-- Unicode &#127760; -->
 					<?php echo $TITLE; ?></a>
 				</div>
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -534,7 +530,7 @@ tr.highlight {background-color: powderblue;}
 	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 	        <li class="nav-item">
-						<a class="nav-link <?php if($ENDPOINT == $MAIN_ENDPOINT) echo "active"; ?>" href="<?php echo overviewlink(); ?>" aria-current="page" >Bitcoin</a>
+						<a class="nav-link <?php if($ENDPOINT == $MAIN_ENDPOINT) echo "active"; ?>" href="<?php echo overviewlink(); ?>" aria-current="page" >Scash</a>
 	        </li>
 	        <li class="nav-item">
 						<a class="nav-link <?php if($ENDPOINT == $TEST_ENDPOINT) echo "active"; ?>" href="<?php echo overviewlink(true); ?>" aria-current="page" >Testnet</a>
@@ -556,7 +552,7 @@ tr.highlight {background-color: powderblue;}
             <hr class="d-lg-none my-2 text-white-50">
           </li>
 	        <li class="nav-item py-1 px-0 px-lg-2">
-						<a href="https://github.com/jonasschnelli/simple-block-explorer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="bi bi-github" viewBox="0 0 16 16">
+						<a href="https://github.com/scash-project/simple-block-explorer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="bi bi-github" viewBox="0 0 16 16">
 						  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
 						</svg></a>
 	        </li>
@@ -859,6 +855,27 @@ tr.highlight {background-color: powderblue;}
 						<?php echo btcamount(($block->stats->totalfee+$block->stats->subsidy)/100000000); ?>
 			    </div>
 			  </li>
+
+
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">Nonce</div>
+						<?php echo $block->nonce; ?>
+			    </div>
+			  </li>
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">Bits</div>
+						0x<?php echo $block->bits; ?>
+			    </div>
+			  </li>
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">Difficulty</div>
+						<?php echo $block->difficulty; ?>
+			    </div>
+			  </li>
+
 			  <li class="list-group-item d-flex justify-content-between align-items-start">
 			    <div class="ms-2 me-auto">
 			      <div class="fw-bold">Previous Block</div>
@@ -913,6 +930,38 @@ tr.highlight {background-color: powderblue;}
 						<?php echo $block->stats->medianfee; ?> sats
 			    </div>
 			  </li>
+
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">RandomX Epoch</div>
+						<?php echo $block->rx_epoch; ?>
+			    </div>
+			  </li>
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">RandomX Hash</div>
+			      <span><?php echo shortHash($block->rx_hash); ?></span>
+						<a href="#" data-bs-toggle="collapse" data-bs-target="#collapseRxHash" aria-expanded="false" aria-controls="collapseRxHash">(Show full hash)</a>
+						<div class="collapse" id="collapseRxHash">
+						  <div class="" style="font-size:.7em">
+						    <?php echo $block->rx_hash; ?>
+						  </div>
+						</div>
+			    </div>
+			  </li>
+			  <li class="list-group-item d-flex justify-content-between align-items-start">
+			    <div class="ms-2 me-auto">
+			      <div class="fw-bold">RandomX Commitment</div>
+			      <span><?php echo shortHash($block->rx_cm); ?></span>
+						<a href="#" data-bs-toggle="collapse" data-bs-target="#collapseRxCm" aria-expanded="false" aria-controls="collapseRxCm">(Show full commitment)</a>
+						<div class="collapse" id="collapseRxCm">
+						  <div class="" style="font-size:.7em">
+						    <?php echo $block->rx_cm; ?>
+						  </div>
+						</div>
+			    </div>
+			  </li>
+
 			  <li class="list-group-item d-flex justify-content-between align-items-start">
 			    <div class="ms-2 me-auto">
 			      <div class="fw-bold">Next Block</div>
